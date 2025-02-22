bf718890d0dc4276e1e21a617225c06652ef20a6
fummyfummy <fuminaga115@g.ecc.u-tokyo.ac.jp>
2025-02-22 19:10:59 +0900

    Deploy latest build to GitHub Pages

--- node_modules/db0/connectors/better-sqlite3.d.ts
+++
@@ -1,8 +0,0 @@
-import Database from "better-sqlite3";
-import type { Connector } from "../types";
-export interface ConnectorOptions {
-    cwd?: string;
-    path?: string;
-    name?: string;
-}
-export default function sqliteConnector(opts: ConnectorOptions): Connector<Database.Database>;
