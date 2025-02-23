ead02bdc42c6323f876ce88b6d21b9d119b94693
fummyfummy <fuminaga115@g.ecc.u-tokyo.ac.jp>
2025-02-23 18:01:41 +0900

    containerの中央揃え。

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,23 @@
 <template>
-  <div class="container">
-    <h1>
-      <T v="[東大](とうだい) [CAST](きゃすと) [五月祭](ごがつさい)2025"></T>
-    </h1>
-    <div class="text-lg md_text-3xl my-1">
-      <span class="text-sm md_text-base">5</span><span class="text-xs"><T v="[月](がつ)"></T></span>
-      <span class="text-sm md_text-base ml-1">24</span><span class="text-xs"><T v="[日](にち)"></T></span>
-      <span class="text-xs bg-blue-600 text-white mx-2 p-1 rounded">土</span>
-      <font-awesome-icon class="mx-2 text-lg" :icon="['fas', 'caret-right']"></font-awesome-icon>
-      <span class="text-sm md_text-base">25</span><span class="text-xs"><T v="[日](にち)"></T></span>
-      <span class="text-xs bg-red-600 text-white mx-2 p-1 rounded">日</span>
-    </div>
-    <div>
-      <T v="「なるほど[実感](じっかん)サイエンスミュージアム」"></T>
-      <T v="「なるほど[体感](たいかん)サイエンスショー」"></T>
-      <T v="「[作](つく)って[体感](たいかん)サイエンスアトリエ」"></T>
-      <T v ="を[出展](しゅってん)しています！"></T>
+  <div class="flex justify-center bg-gray-100">
+    <div class="container flex flex-col items-center justify-center p-4 bg-white shadow-md rounded-lg my-8">
+      <h1>
+        <T v="[東大](とうだい) [CAST](きゃすと) [五月祭](ごがつさい)2025"></T>
+      </h1>
+      <div class="text-lg md:text-3xl my-1">
+        <span class="text-sm md:text-base">5</span><span class="text-xs"><T v="[月](がつ)"></T></span>
+        <span class="text-sm md:text-base ml-1">24</span><span class="text-xs"><T v="[日](にち)"></T></span>
+        <span class="text-xs bg-blue-600 text-white mx-2 p-1 rounded">土</span>
+        <font-awesome-icon class="mx-2 text-lg" :icon="['fas', 'caret-right']"></font-awesome-icon>
+        <span class="text-sm md:text-base">25</span><span class="text-xs"><T v="[日](にち)"></T></span>
+        <span class="text-xs bg-red-600 text-white mx-2 p-1 rounded">日</span>
+      </div>
+      <div>
+        <T v="「なるほど[実感](じっかん)サイエンスミュージアム」"></T>
+        <T v="「なるほど[体感](たいかん)サイエンスショー」"></T>
+        <T v="「[作](つく)って[体感](たいかん)サイエンスアトリエ」"></T>
+        <T v="を[出展](しゅってん)しています！"></T>
+      </div>
     </div>
   </div>
 </template>
@@ -37,17 +39,5 @@ export default {
 </script>
 
 <style scoped>
-.container {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 1rem;
-  background-color: #f0f0f0; 
-}
-
-@media (min-width: 768px) {
-  .md_text-3xl {
-    font-size: 1.875rem;
-  }
-}
+/* Tailwind CSSのユーティリティクラスを使用するため、スタイル設定は不要です */
 </style>
