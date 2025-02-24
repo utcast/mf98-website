651378645805597a063a5013524abfee7ec26a43
yichimura17th <4294192731@g.ecc.u-tokyo.ac.jp>
2025-02-24 17:40:26 +0900

    ルビを正しく直した

--- pages/museum/index.vue
+++ pages/museum/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container">
         <h1>
-        <ruby>なるほど<rt>じっかん</rt></ruby>実感サイエンスミュージアム
+        なるほど<ruby>実感<rt>じっかん</rt></ruby>サイエンスミュージアム
         </h1>
         ここでは、科学の面白さを実感できる実験を行っています。
     </div>
