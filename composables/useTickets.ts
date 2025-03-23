c9afe84d942c16bcfeff65a39f14ccc888e46ba6
fummyfummy <fuminaga115@g.ecc.u-tokyo.ac.jp>
2025-03-23 10:09:39 +0900

    アトリエ整理券をすぷしで管理

--- composables/useTickets.ts
+++ composables/useTickets.ts
@@ -1,12 +1,32 @@
-import ticketData from '@/static/json/tickets.json'
+import { ref, onMounted } from 'vue'
 
-type TicketData = {
-  [key: string]: { reserved: number; limit: number }
+interface Ticket {
+  id: number
+  reserved: number
+  limit: number
 }
+
+const ticketData = ref<Ticket[]>([])
 
 export const useTickets = () => {
-  const getTicketStatus = (id: number) => {
-    return (ticketData as TicketData)[id.toString()] || { reserved: 0, limit: 0 }
+  const fetchTickets = async () => {
+    try {
+      const res = await fetch('https://script.google.com/macros/s/AKfycby5suexYWHerSXvPKLqzaUEkquTrOPP716e_1DTq3V84YxSMwq3xtW5lZ_oYiOXkwZY/exec')
+      ticketData.value = await res.json()
+    } catch (err) {
+      console.error('チケットデータの取得に失敗しました:', err)
+    }
   }
-  return { getTicketStatus }
+
+  const getTicketStatus = (id: number) => {
+    return ticketData.value.find((t) => t.id === id) || { reserved: 0, limit: 0 }
+  }
+
+  // 自動でAPIをコール（onMountedでもOK）
+  onMounted(() => {
+    fetchTickets()
+  })
+
+  return { fetchTickets, getTicketStatus }
 }
+
