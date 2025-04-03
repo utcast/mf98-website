0bf96f87ee0e2e9c3e6b98b976e0f0069dbeec31
fummyfummy <fuminaga115@g.ecc.u-tokyo.ac.jp>
2025-04-03 18:18:17 +0900

    君S整理券ページ用ページ作成。

---
+++ components/kimiSNav.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="museum-nav bg-white shadow-lg p-4 text-center max-w-6xl mx-auto">
+      <!-- ロゴ画像を表示 -->
+      <h1 class="text-4xl font-bold text-center">君もSCをしてみよう</h1>
+            <nav class="my-4 justify-center">
+        <ul class="w-4/5 flex justify-center mx-auto">
+          <li class="mr-5 bg-gray-50" :class="{ 'tab-active-li': tabActive === '1_1' }">
+            <nuxt-link to="/kimiS/1_1" class="tab-youtube" :class="[tabActive === '1_1' ? 'tab-active' : 'tab-inactive']">
+              <T v="[1日目](いちにちめ) [午前](ごぜん)" />
+            </nuxt-link>
+          </li>
+          <li class="mr-5 bg-gray-50" :class="{ 'tab-active-li': tabActive === '1_2' }">
+            <nuxt-link to="/kimiS/1_2" class="tab-youtube" :class="[tabActive === '1_2' ? 'tab-active' : 'tab-inactive']">
+              <T v="[1日目](いちにちめ) [午後](ごご)" />
+            </nuxt-link>
+          </li>
+          <li class="mr-5 bg-gray-50" :class="{ 'tab-active-li': tabActive === '2_1' }">
+            <nuxt-link to="/kimiS/2_1" class="tab-youtube" :class="[tabActive === '2_1' ? 'tab-active' : 'tab-inactive']">
+              <T v="[2日目](ふつかめ) [午前](ごぜん)" />
+            </nuxt-link>
+          </li>
+          <li class="mr-5 bg-gray-50" :class="{ 'tab-active-li': tabActive === '2_2' }">
+            <nuxt-link to="/kimiS/2_2" class="tab-youtube" :class="[tabActive === '2_2' ? 'tab-active' : 'tab-inactive']">
+              <T v="[2日目](ふつかめ) [午後](ごご)" />
+            </nuxt-link>
+          </li>
+        </ul>
+        <slot></slot>
+      </nav>
+    </div>
+  </template>
+  
+  <script setup>
+  import T from '~/components/T.vue'
+  </script>
+  
+  <script>
+  export default {
+    scrollToTop: true,
+    computed: {
+      tabActive() {
+        if (this.$route.name.match(/1_1/)) {
+          return '1_1';
+        } else if (this.$route.name.match(/1_2/)) {
+          return '1_2';
+        } else if (this.$route.name.match(/2_1/)) {
+          return '2_1';
+        } else {
+          return '2_2';
+        }
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  /* Tailwind CSSのユーティリティクラスを使用するため、スタイル設定は不要です */
+  </style>
---
+++ pages/kimiS/1_1.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+      <kimiSNav>
+        <div class="tab_museum w-4/5 bg-gray-50 p-6 shadow-md mx-auto">
+          <div class="flex justify-center items-center space-x-4 my-3">
+            <h1 class="text-4xl font-bold text-center">君もSCをしてみよう</h1>
+          </div>
+          <div class="content-container flex flex-col items-center w-5/6 mx-auto">
+            <p>ここにスプレッドシートから読み込んだ内容を表示します。</p>
+          </div>
+        </div>
+      </kimiSNav>
+    </div>
+</template>
+
+<script setup>
+import kimiSNav from '~/components/kimiSNav.vue';
+</script>
+
+<style scoped>
+.content-container {
+  display: flex;
+  gap: 10px;
+}
+</style>
