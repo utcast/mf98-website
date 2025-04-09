631e38ea911ff3fb27b9fb5e231961a610ec9101
aohira16 <lajward16@g.ecc.u-tokyo.ac.jp>
2025-04-09 17:45:49 +0900

    アクセスの動画のサイズ等修正

--- pages/access.vue
+++ pages/access.vue
@@ -1,59 +1,62 @@
 <template>
-  <div>
     <h1 class="text-5xl text-center">アクセス</h1>
-    <div>
-      <div class="triangle-container">
-        <TitleTriangle title="ミュージアム[会場](かいじょう)" position="left" />
-        <div class="iframe-container">
-          <iframe
-            class="responsive-iframe"
-            src="https://www.youtube.com/embed/r2nEBncRujw?si=975L5zIrpXM-kuBR" 
-            frameborder="0"
-            allowfullscreen
-          ></iframe>
-        </div>
-      </div>
-
-      <div class="flex justify-end">
-        <div class="triangle-container">
-          <TitleTriangle title="ショー[会場](かいじょう)" position="right" />
-          <div class="iframe-container">
-            <iframe
-              class="responsive-iframe"
-              src="https://www.youtube.com/embed/r2nEBncRujw?si=975L5zIrpXM-kuBR" 
-              frameborder="0"
-              allowfullscreen
-            ></iframe>
-          </div>
-        </div>
-      </div>
-
-      <div class="triangle-container">
-        <TitleTriangle title="アトリエ[会場](かいじょう)" position="left" />
-        <div class="iframe-container">
-          <iframe
-            class="responsive-iframe"
-            src="https://www.youtube.com/embed/r2nEBncRujw?si=975L5zIrpXM-kuBR" 
-            frameborder="0"
-            allowfullscreen
-          ></iframe>
-        </div>
+    <div class="triangle-container">
+      <TitleTriangle title="ミュージアム[会場](かいじょう)" position="left" />
+      <div class="iframe-container">
+        <iframe
+          class="responsive-iframe"
+          src="https://www.youtube.com/embed/r2nEBncRujw?si=975L5zIrpXM-kuBR" 
+          frameborder="0"
+          allowfullscreen
+        ></iframe>
       </div>
     </div>
-  </div>
+
+    <div class="triangle-container-right">
+      <TitleTriangle title="ショー[会場](かいじょう)" position="right" />
+      <div class="iframe-container">
+        <iframe
+          class="responsive-iframe"
+          src="https://www.youtube.com/embed/r2nEBncRujw?si=975L5zIrpXM-kuBR" 
+          frameborder="0"
+          allowfullscreen
+        ></iframe>
+      </div>
+    </div>
+    <div class="triangle-container">
+      <TitleTriangle title="アトリエ[会場](かいじょう)" position="left" />
+      <div class="iframe-container">
+        <iframe
+          class="responsive-iframe"
+          src="https://www.youtube.com/embed/r2nEBncRujw?si=975L5zIrpXM-kuBR" 
+          frameborder="0"
+          allowfullscreen
+        ></iframe>
+      </div>
+    </div>
 </template>
 
 <style scoped>
 .triangle-container {
   position: relative;
-  width: fit-content;
-  margin-bottom: 200px; /* 重なりのための余白 */
+  width: 100%;
+  aspect-ratio: 16/9;/*縦横比を固定*/
+  margin-top: 50px; /* 重なりのための余白 */
+}
+
+.triangle-container-right {
+  position: relative;
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;/*右に揃える*/ 
+  aspect-ratio: 16/9;
+  margin-top: 50px; /* 重なりのための余白 */
 }
 
 /* iframe を適切な位置に配置 */
 .iframe-container {
   position: absolute;
-  top: 100px; /* 50px の空き + 150px の重なり */
+  top: 70px; 
   left: 50%;
   transform: translateX(-50%);
   width: 100%;
