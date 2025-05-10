19f2a3c3446ccc1056f4930cb04879411e452a38
fummyfummy <fuminaga115@g.ecc.u-tokyo.ac.jp>
2025-05-10 11:20:13 +0900

    光の箱中心に加筆

--- pages/atelier/box.vue
+++ pages/atelier/box.vue
@@ -3,12 +3,13 @@
     <AtelierNav>
       <div class="tab_museum w-4/5 bg-gray-50 p-6 shadow-md mx-auto ">
         <div class="flex justify-center item-center space-x-4 my-3">
-          <img src="" alt="光の箱ロゴ" class="object-contain h-11" />
+          <img src="@/assets/img/box_logo.png" alt="光の箱ロゴ" class="object-contain h-11" />
           <h1 class="text-4xl font-bold text-center">光の箱</h1>
         </div>
         <div class="card-container flex flex-col items-center w-5/6 mx-auto">
           <Card v-for="(card, index) in filteredCards" :key="index" :title="card.title" :content="card.content" :img="card.img" />
         </div>
+        <small>光の箱は松村泰三さんが考案した工作です。</small>
       </div>
     </AtelierNav>
   </div>
--- pages/atelier/pendulum.vue
+++ pages/atelier/pendulum.vue
@@ -3,7 +3,7 @@
     <AtelierNav>
       <div class="tab_museum w-4/5 bg-gray-50 p-6 shadow-md mx-auto ">
         <div class="flex justify-center item-center space-x-4 my-3">
-          <img src="" alt="念力振り子ロゴ" class="object-contain h-11" />
+          <img src="@/assets/img/huriko_logo.png" alt="念力振り子ロゴ" class="object-contain h-11" />
           <h1 class="text-4xl font-bold text-center">念力振り子</h1>
         </div>
         <div class="card-container flex flex-col items-center w-5/6 mx-auto">
