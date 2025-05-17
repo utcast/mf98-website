b0b49fd6ea743c72c826220eea27e910b794c424
fummyfummy <fuminaga115@g.ecc.u-tokyo.ac.jp>
2025-05-17 12:05:57 +0900

    念力振り子ミス修正

--- pages/en/atelier/pendulum.vue
+++ pages/en/atelier/pendulum.vue
@@ -5,7 +5,7 @@
         <div class="flex justify-center item-center space-x-4 my-3">
           <img src="@/assets/img/huriko_logo.png" alt="念力振り子ロゴ" class="object-contain h-11" />
           <h1 class="text-4xl font-bold text-center">
-            <T :v="`[念力振](ねんりきふ)り[子](こ)`" />
+            <T :v="`telekinetic pendulum`" />
           </h1>
         </div>
         <div class="card-container flex flex-col items-center w-5/6 mx-auto">
